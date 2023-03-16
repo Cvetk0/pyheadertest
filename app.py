d671b106882f580379c7d9e5aadf270e91b64e24
Jernej Kladnik <jernej.kladnik@adacta-fintech.com>
2023-03-16 13:12:29 +0700

    Update

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+app = Flask(__name__)
 
-hostName = "localhost"
-serverPort = 8080
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes(f"<p>Request: {self.path}</p>", "utf-8"))
-        self.wfile.write(bytes(f"<p>Headers:</br>{'<br>'.join([f'{h}: {self.headers.get(h)}' for h in self.headers])}</p>", "utf-8"))
-        self.wfile.write(bytes(f"</html>", "utf-8"))
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return f"{'<br>'.join([f'{k[0]}: {k[1]}' for k in request.headers])}"
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+if __name__ == '__main__':
+   app.run()
